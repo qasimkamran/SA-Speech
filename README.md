5f204062b8cb56683fa854f9800a920e87e96395
qasimkamran <qasimworkaus@gmail.com>
2023-05-23 14:10:47 +0000

    Update notebook script perfectly functional and passing

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 - https://drive.google.com/drive/folders/1_guNIMc49P5EWWZca5Av7HHd6FSrBjfK?usp=share_link
 
 ### Notebooks
-- https://colab.research.google.com/drive/130taDfOzsP43hRQDnQMBlU9VGhEZLXbj?usp=share_link
+- https://colab.research.google.com/drive/130taDfOzsP43hRQDnQMBlU9VGhEZLXbj?usp=sharing
 - https://colab.research.google.com/drive/15ICUlw8xNFftnUwRnJi0HkIabjX2W2ZG?usp=share_link
--- scripts/update_notebooks.py
+++ scripts/update_notebooks.py
@@ -1,4 +1,7 @@
 import gdown
+import shutil
+import io
+import re
 
 
 README_FILE = 'README.md'
@@ -6,27 +9,26 @@ README_FILE = 'README.md'
 
 def get_notebook_drive_links():
     notebook_links = []
-    flag = '### Notebooks'
-    flag_found = False
+    with open(README_FILE, "r") as readme_file:
+        readme_content = readme_file.read()
+    notebooks_flag = re.search(r"### Notebooks\n", readme_content)
 
-    with open(README_FILE, 'r') as file:
-        for line in file:
-            line = line.strip()
-
-            print(line)
-
-            if flag_found and line.startswith('- https://colab.research.google.com/drive/'):
-                notebook_links.append(line)
-
-            if line == flag:
-                flag_found = True
-            elif line and not line.startswith('#'):
-                flag_found = False
+    if notebooks_flag:
+        links = re.findall(r"- (.*?)\n", readme_content[notebooks_flag.end():])
+        notebook_links = [link.strip() for link in links]
 
     return notebook_links
 
 
+def download_notebook(share_link):
+    file_id = share_link.split("/drive/")[1].split("/")[0]
+    file_id = file_id.split("?")[0]
+    download_link = 'https://drive.google.com/uc?id='+ file_id
+    filename = gdown.download(download_link)
+    destination = 'notebooks/' + filename
+    shutil.move(filename, destination)
+
 if __name__ == '__main__':
     notebook_links = get_notebook_drive_links()
     for link in notebook_links:
-       gdown.download(link)
+        download_notebook(link)
