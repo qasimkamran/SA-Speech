84e6adaf0f1e9aecf5491ba0396bea3bca6913b2
qasimkamran <qasimworkaus@gmail.com>
2023-05-24 15:56:50 +0000

    Placeholder mfcc and mel spec files added along with modified prepare script

--- scripts/prepare_mfcc.py
+++ scripts/prepare_mfcc.py
@@ -1,17 +1,24 @@
 import os
+import resampy
 import librosa
+import numpy as np
+import pandas as pd
 
 
-def get_mfcc_features(file_path: str, mfcc_len: int = 13, duration: int = 2.5):
-  signal, fs = librosa.load(file_path, res_type='kaiser_fast', duration=duration, sr=22050*2, offset=0.5)
+def get_mfcc(directory: str, mfcc_len: int = 13, duration: int = 2.5):
+  signal, fs = librosa.load(directory, res_type='kaiser_fast', duration=duration, sr=22050*2, offset=0.5)
   s_len = len(signal)
   if s_len < duration * fs:
     padding = int(duration * fs) - s_len
     signal = librosa.util.pad_center(signal, padding)
   mfcc = librosa.feature.mfcc(y=signal, sr=fs, n_mfcc=mfcc_len)
   mfcc = mfcc.T
-  features = mfcc
-  return features
+  return mfcc
+
+
+def get_mean_mfcc_per_frame(directory):
+  mfcc = get_feature(directory)
+  mean_mfcc = np.mean(mfcc, axis=1)
 
 
 def get_n_wav_files(directory, n):
@@ -26,5 +33,3 @@ def get_n_wav_files(directory, n):
           return wav_files
   return wav_files
 
-
-if __name__ == '__main__':
