4cc0100f0394995f242629d263f0184d8d56bf81
qasimk <qasimworkaus@gmail.com>
2023-06-20 12:35:53 +0100

    Prepare data script complete and functional

--- scripts/data_prepare.py
+++ scripts/data_prepare.py
@@ -1,56 +1,68 @@
 import os
 import argparse
 import wave
-import shutil
-import random
+import csv
 import transcriptor
-import numpy as np
 import pandas as pd
 
-parser = argparse.ArgumentParser(description='Argument Parser')
 
-parser.add_argument('--filepath', dest='filepath', action='store', required=False, type=str)
+REQUIRED_COLUMNS = ['StartTime(s)', 'EndTime(s)', 'EXCITED', 'CHEERING', 'DISAPPOINTED', 'BOOING', 'ANGRY', 'APPLAUSE', 'SINGING', 'PA']
 
-args = parser.parse_args()
 
-file_list = os.listdir(args.filepath)
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Argument Parser')
+    parser.add_argument('--datadir', dest='datadir', action='store', required=True, type=str)
+    parser.add_argument('--outputdir', dest='outputdir', action='store', required=True, type=str)
+    return parser.parse_args()
 
-if file_list != 2:
-    print('Data directory must only contain two files')
-    raise Exception
 
-wav_files = [file for file in file_list if file.endswith('.wav')]
-csv_files = [file for file in file_list if file.endswith('.csv')]
+def validate_files(file_list):
+    if len(file_list) != 2:
+        print(f'Data directory must only contain two files. Current count: {len(file_list)}')
+        raise ValueError('Invalid file count in directory.')
 
-if len(wav_files) != 1 and len(csv_files) != 1:
-    print('There must only be one CSV and only one WAV file in the data directory')
-    raise Exception
+    wav_files = [file for file in file_list if file.endswith('.wav')]
+    csv_files = [file for file in file_list if file.endswith('.csv')]
 
-required_columns = ['StartTime(s)', 'EndTime(s)', 'EXCITED', 'CHEERING', 'DISAPPOINTED', 'BOOING', 'ANGRY', 'APPLAUSE', 'SINGING', 'PA']
+    if len(wav_files) != 1 or len(csv_files) != 1:
+        print('There must only be one CSV and one WAV file in the data directory.')
+        raise ValueError('Invalid file types in directory.')
+    return wav_files[0], csv_files[0]
 
-with open(csv_files, 'r') as file:
-    csv_reader = csv.reader(file)
-    csv_header = next(reader) 
 
-if all(column in csv_header for column in required_columns):
-    print("CSV file contains the required columns.")
-else:
-    print("CSV file does not contain all the required columns.")
-    raise Exception
+def validate_csv(csv_header):
+    if all(column in csv_header for column in REQUIRED_COLUMNS):
+        print("CSV file contains the required columns.")
+    else:
+        print("CSV file does not contain all the required columns.")
+        raise ValueError('CSV file missing required columns.')
 
-df = pd.read_csv(csv_files)
-original_audio = wave.open(wav_files, "rb")
-original_sample_rate = original_audio.getframerate()
 
-for index, row in df.iterrows():
-  start_time_s = int(row['StartTime(s)'])
-  end_time_s = int(row['EndTime(s)'])
-  transcriptor.clip_audio(original_audio, start_time_s, (end_time_s - start_time_s), '{0}.wav'.format(index))
-  with open('SOUNDFIELD_C_{0}.txt'.format(index), "wb") as label_file:
-    line = str(float(row['EXCITED'])) + ' ' + str(float(row['CHEERING'])) + ' ' + \
-           str(float(row['DISAPPOINTED'])) + ' ' + str(float(row['BOOING'])) + ' ' + \
-           str(float(row['ANGRY'])) + ' ' + str(float(row['APPLAUSE'])) + ' ' + \
-           str(float(row['SINGING'])) + ' ' + str(float(row['PA']))
-    encoded_line = line.encode('utf-8')
-    label_file.write(encoded_line)
-    print('Written {0}.txt'.format(index))
+def main():
+    args = parse_arguments()
+    file_list = os.listdir(args.datadir)
+    wav_file, csv_file = validate_files(file_list)
+    wav_path = os.path.join(args.datadir, wav_file)
+    csv_path = os.path.join(args.datadir, csv_file)
+
+    with open(csv_path, 'r') as file:
+        csv_reader = csv.reader(file)
+        csv_header = next(csv_reader)
+    validate_csv(csv_header)
+
+    df = pd.read_csv(csv_path)
+    original_audio = wave.open(wav_path, "rb")
+
+    os.chdir(args.outputdir)
+    for index, row in df.iterrows():
+        start_time_s = int(row['StartTime(s)'])
+        end_time_s = int(row['EndTime(s)'])
+        transcriptor.clip_audio(original_audio, start_time_s, (end_time_s - start_time_s), f'{index}.wav')
+        labels = ' '.join(str(float(row[column])) for column in REQUIRED_COLUMNS[2:])
+        with open(f'{index}.txt', "wb") as label_file:
+            label_file.write(labels.encode('utf-8'))
+            print(f'Written to {index}.txt')
+
+
+if __name__ == "__main__":
+    main()
