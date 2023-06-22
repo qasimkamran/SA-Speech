aaae5f74b8f9954c60892ffc78a2aa915c3763e9
qasimk <qasimworkaus@gmail.com>
2023-06-22 10:20:55 +0100

    Temporary commit

--- scripts/data_prepare.py
+++ scripts/data_prepare.py
@@ -4,6 +4,8 @@ import wave
 import csv
 import transcriptor
 import pandas as pd
+import numpy as np
+import soundfile as sf
 
 
 REQUIRED_COLUMNS = ['StartTime(s)', 'EndTime(s)', 'EXCITED', 'CHEERING', 'DISAPPOINTED', 'BOOING', 'ANGRY', 'APPLAUSE', 'SINGING', 'PA']
@@ -11,8 +13,8 @@ REQUIRED_COLUMNS = ['StartTime(s)', 'EndTime(s)', 'EXCITED', 'CHEERING', 'DISAPP
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Argument Parser')
-    parser.add_argument('--datadir', dest='datadir', action='store', required=True, type=str)
-    parser.add_argument('--outputdir', dest='outputdir', action='store', required=True, type=str)
+    parser.add_argument('--data_dir', dest='datadir', action='store', required=True, type=str)
+    parser.add_argument('--output_dir', dest='outputdir', action='store', required=True, type=str)
     return parser.parse_args()
 
 
@@ -36,6 +38,75 @@ def validate_csv(csv_header):
     else:
         print("CSV file does not contain all the required columns.")
         raise ValueError('CSV file missing required columns.')
+
+
+def pad_audio(audio, target_duration, sr):
+    print(f'Sample width: {audio.getsampwidth()}')
+    audio_frames = audio.readframes(audio.getnframes())
+    audio_array = np.frombuffer(audio_frames, dtype=np.float64)  # Assuming 16-bit PCM audio
+    audio_duration = audio.getnframes() / sr
+
+    print(f'Sample rate: {sr}')
+    print(f'Audio duration: {audio_duration}')
+    print(f'Target duration: {target_duration}')
+
+    if audio_duration < target_duration:
+        total_samples_required = int(target_duration * sr)
+        audio_full_repeats = np.tile(audio_array, int(total_samples_required / audio_array.size))
+        remaining_samples = total_samples_required - audio_full_repeats.size
+        audio_partial_repeat = audio_array[:remaining_samples]
+        padded_audio_array = np.concatenate([audio_full_repeats, audio_partial_repeat])
+        print(f'Size: {padded_audio_array.size}')
+        return padded_audio_array.tobytes()
+    else:
+        return audio_frames
+
+
+def open_audio_file(audio_dir):
+    audio_file = wave.open(audio_dir, "rb")
+    audio_name, _ = os.path.splitext(audio_dir)
+    audio_params = (audio_file.getnchannels(),
+                    audio_file.getsampwidth(),
+                    audio_file.getframerate(),
+                    audio_name)
+    n_frames = audio_file.getnframes()
+    duration = n_frames / float(audio_params[2])
+    return audio_file, audio_params, duration
+
+
+def create_clips(audio_file, audio_params, duration, clip_duration):
+    clip_names = []
+    n_splits = int(duration / clip_duration) + 1
+    audio_name = audio_params[-1]
+
+    for i in range(n_splits):
+        startpos = i * clip_duration
+        remainder = duration - startpos
+        clip_name = '{0}_{1}.wav'.format(audio_name, i)
+
+        if remainder < clip_duration and remainder != 0:
+            process_last_clip(audio_file, audio_params, startpos, clip_duration, clip_name)
+            break
+
+        transcriptor.clip_audio(audio_file, startpos, clip_duration, clip_name)
+        clip_names.append(clip_name)
+
+    return clip_names
+
+
+def process_last_clip(audio_file, audio_params, startpos, clip_duration, clip_name):
+    transcriptor.clip_audio(audio_file, startpos, clip_duration, clip_name)
+    with wave.open(clip_name, "rb") as audio_clip:
+        audio_clip = pad_audio(audio_clip, clip_duration , audio_params[2])
+    os.remove(clip_name)
+    sf.write(clip_name, audio_clip, audio_params[2], subtype='PCM_24')
+
+
+def homogenize_clips(audio_dir, label_dir, clip_duration):
+    audio_file, audio_params, duration = open_audio_file(audio_dir)
+    clip_names = create_clips(audio_file, audio_params, duration, clip_duration)
+    audio_file.close()
+    return clip_names
 
 
 def main():
@@ -62,7 +133,11 @@ def main():
         with open(f'{index}.txt', "wb") as label_file:
             label_file.write(labels.encode('utf-8'))
             print(f'Written to {index}.txt')
+        homogenize_clips(f'{index}.wav', f'{index}.txt', 3)
+        os.remove(f'{index}.wav')
 
 
 if __name__ == "__main__":
     main()
+    # last_clip = wave.open('data/1_1.wav', "rb")
+    # print(last_clip.getnframes() / last_clip.getframerate())
--- scripts/mfcc_prepare.py
+++ scripts/mfcc_prepare.py
@@ -5,40 +5,51 @@ import numpy as np
 import pandas as pd
 import argparse
 
-parser = argparse.ArgumentParser(description='Argument Parser')
 
-parser.add_argument('--data_dir', dest='data_dir', action='store', required=True, type=str)
-parser.add_argument('--mfcc_len', dest='mfcc_len', action='store', type=int)
-parser.add_argument('--duration', dest='duration', action='store', type=float)
-parser.add_argument('--mode', dest='mode', action='store', required=True, type=str)
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Argument Parser')
+    parser.add_argument('--data_dir', dest='data_dir', action='store', required=True, type=str)
+    parser.add_argument('--mfcc_len', dest='mfcc_len', action='store', type=int)
+    parser.add_argument('--duration', dest='duration', action='store', type=float)
+    parser.add_argument('--mode', dest='mode', action='store', required=True, type=str)
+    return parser.parse_args()
 
-args = parser.parse_args()
 
 def get_mfcc(directory: str, mfcc_len: int = 13, duration: int = 2.5):
-  signal, fs = librosa.load(directory, res_type='kaiser_fast', duration=duration, sr=22050*2, offset=0.5)
-  s_len = len(signal)
-  if s_len < duration * fs:
-    padding = int(duration * fs) - s_len
-    signal = librosa.util.pad_center(signal, padding)
-  mfcc = librosa.feature.mfcc(y=signal, sr=fs, n_mfcc=mfcc_len)
-  mfcc = mfcc.T
-  return mfcc
+    signal, fs = librosa.load(directory, res_type='kaiser_fast', duration=duration, sr=22050*2, offset=0.5)
+    s_len = len(signal)
+    if s_len < duration * fs:
+        padding = int(duration * fs) - s_len
+        signal = librosa.util.pad_center(signal, padding)
+    mfcc = librosa.feature.mfcc(y=signal, sr=fs, n_mfcc=mfcc_len)
+    mfcc = mfcc.T
+    return mfcc
 
 
 def get_mean_mfcc_per_frame(directory: str):
-  mfcc = get_feature(directory)
-  mean_mfcc = np.mean(mfcc, axis=1)
+    mfcc = get_mfcc(directory)
+    mean_mfcc = np.mean(mfcc, axis=1)
 
 
-def get_n_wav_files(directory: str, n: int):
-  wav_files = []
-  count = 0
-  for root, dirs, files in os.walk(directory):
-    for file in files:
-      if file.endswith(".wav"):
-        wav_files.append(os.path.join(root, file))
-        count += 1
-        if count == n:
-          return wav_files
-  return wav_files
+def get_n_wav_files(directory: str, n: int = None):
+    wav_files = []
+    count = 0
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".wav"):
+                wav_files.append(os.path.join(root, file))
+                count += 1
+                if count == n:
+                    return wav_files
+    return wav_files
 
+
+def main():
+    args = parse_arguments()
+    wav_files = get_n_wav_files(args.data_dir)
+    mfcc_list = []
+    if args.mode == 'mean':
+        mfcc_list.append()
+
+if __name__ == '__main__':
+    main()
